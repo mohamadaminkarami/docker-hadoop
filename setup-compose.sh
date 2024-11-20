e3d9f19f1201913baa4886df8a596320c0480824
Mohamadamin Karami <mohamadamin.karami@divar.ir>
2024-11-21 00:21:09 +0330

    Update docker-compose.yml to pull images from Docker Hub

--- setup-compose.sh
+++ setup-compose.sh
@@ -5,8 +5,5 @@ else
     export RELEASE="latest"
 fi
 
-## TODO: This should not build the images by default, but should just download them
-make build
-
 # https://docs.docker.com/compose/migrate
 docker-compose up -d || docker compose up -d
