9fd5c15853f07eec0a2daf6b37331554fef9836d
Mohamadamin Karami <mohamadamin.karami@divar.ir>
2024-11-21 15:31:08 +0330

    Add --rebuild argument

--- setup-compose.sh
+++ setup-compose.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-if [ "$1" == '--eval' ]; then
-    export RELEASE="eval"
-else
-    export RELEASE="latest"
-fi
 
+# Initialize variables
+export RELEASE="latest"
+export SOURCE="binpash/"
+
+for arg in "$@"; do
+    if [ "$arg" == '--rebuild' ]; then
+        export RELEASE="eval"
+        export SOURCE=""
+
+        echo "Rebuilding Docker images..."
+        make build || {
+            echo "Build failed"
+            exit 1
+        }
+    fi
+done
+
+# Bring up the services
+echo "Starting Docker Compose services..."
 # https://docs.docker.com/compose/migrate
 docker-compose up -d || docker compose up -d
